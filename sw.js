503d06dc54d4e50bab35d5cb25ff1afee21ea05b
Makoto KUNO <mkt-kuno@iis.u-tokyo.ac.jp>
2026-06-02 18:01:42 +0900

    SWをnetwork-firstに変更しキャッシュv2に更新

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'grading-game-v1';
+const CACHE = 'grading-game-v2';
 const ASSETS = [
   './',
   './index.html',
@@ -18,5 +18,13 @@ self.addEventListener('activate', e => {
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    fetch(e.request)
+      .then(r => {
+        const clone = r.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return r;
+      })
+      .catch(() => caches.match(e.request))
+  );
 });
