f8b88e57b1301a3f3c0cbfdc713a2893d6a482db
Makoto KUNO <mkt-kuno@iis.u-tokyo.ac.jp>
2026-06-04 16:00:16 +0900

    Partial response(206)をキャッシュしないように修正

--- sw.js
+++ sw.js
@@ -24,8 +24,10 @@ self.addEventListener('fetch', e => {
   e.respondWith(
     fetch(e.request)
       .then(r => {
-        const clone = r.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
+        if (r.ok) {
+          const clone = r.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
         return r;
       })
       .catch(() => caches.match(e.request))
